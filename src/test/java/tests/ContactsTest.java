bbf6aaae314c2ad973f1ab4cc0c503d3e0b1490d
kubanych <Minnesota312>
2019-08-13 20:40:24 -0500

    Modified Contacts tests

--- src/test/java/tests/ContactsTest.java
+++ src/test/java/tests/ContactsTest.java
@@ -7,6 +7,9 @@ public class ContactsTest {
     @Test
     public void test1(){
 
+        System.out.println("driver open vytrack");
+        System.out.println("go to contacts page");
+
     }
 
 }
